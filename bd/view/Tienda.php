57a94f413dd57b0c72d9f6da3659125e94228d9f
cristianreey <cris11salle@gmail.com>
2024-01-22 20:39:14 +0100

    codigo con productos arreglados

--- bd/view/Tienda.php
+++ bd/view/Tienda.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
     <title>BioSalud</title>
 </head>
 
