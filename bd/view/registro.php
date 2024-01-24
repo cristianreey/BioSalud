3dec1ee9f62a60498e6a43966a96ffa24440627f
cristianreey <cris11salle@gmail.com>
2024-01-24 13:31:04 +0100

    dods

--- bd/view/registro.php
+++ bd/view/registro.php
@@ -6,38 +6,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>bioSalud</title>
     <link rel="stylesheet" href="../CSS/formularioFlotante.css">
+    <script>
+        function validarFormulario() {
+            var contrasena = document.getElementsByName('contrasena')[0].value;
+            var confirmarContrasena = document.getElementsByName('confirmarContrasena')[0].value;
+
+            if (contrasena !== confirmarContrasena) {
+                alert("Las contraseñas no coinciden");
+                return false;
+            }
+
+            return true;
+        }
+    </script>
 </head>
 
 <body>
     <div class="signupFrm">
-        <form action="" class="form">
+        <form action="../controller/loginController.php" method="POST" class="form"
+            onsubmit="return validarFormulario()">
             <h1 class="title">Regístrate</h1>
 
             <div class="inputContainer">
-                <input type="text" class="input" placeholder="a">
+                <input type="text" name="nombre" class="input" placeholder="a">
                 <label for="" class="label">Nombre completo</label>
             </div>
             <div class="inputContainer">
-                <input type="text" class="input" placeholder="a">
+                <input type="email" name="gmail" class="input" placeholder="a">
                 <label for="" class="label">Email</label>
             </div>
             <div class="inputContainer">
-                <input type="text" class="input" placeholder="a">
+                <input type="text" name="DNI" class="input" placeholder="a">
                 <label for="" class="label">D.N.I</label>
             </div>
             <div class="inputContainer">
-                <input type="text" class="input" placeholder="a">
+                <input type="date" name="fechaNac" class="input" placeholder="a">
                 <label for="" class="label">Fecha de nacimiento</label>
             </div>
             <div class="inputContainer">
-                <input type="text" class="input" placeholder="a">
+                <input type="password" name="contrasena" class="input" placeholder="a">
                 <label for="" class="label">Contraseña</label>
             </div>
             <div class="inputContainer">
-                <input type="text" class="input" placeholder="a">
+                <input type="password" name="confirmarContrasena" class="input" placeholder="a">
                 <label for="" class="label">Confirmar contraseña</label>
             </div>
-
 
             <input type="submit" class="submitBtn" value="Entrar">
         </form>
