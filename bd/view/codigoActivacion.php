53ffc135274513cf71578be8209da8585de645ff
miguel415 <mcremor0512@g.educaand.es>
2024-01-24 16:41:19 +0100

    21

---
+++ bd/view/codigoActivacion.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bioSalud</title>
+    <link rel="stylesheet" href="../CSS/formularioFlotante.css">
+</head>
+
+<body>
+    <div class="signupFrm">
+        <form action="" class="form">
+            <a href="../view/login.html" class="backArrowLink">
+                <img src="../IMAGEN/angulo-izquierdo.png" alt="Flecha hacia atrás">
+            </a>
+            <h1 class="title">¿Eres tú?</h1>
+
+            <div class="inputContainer">
+                <input type="text" name="nombre" class="input" placeholder="a">
+                <label for="" class="label">Inserte el código</label>
+            </div>
+
+            <input type="submit" class="submitBtn" value="Entrar">
+        </form>
+    </div>
+</body>
+
+</html>
