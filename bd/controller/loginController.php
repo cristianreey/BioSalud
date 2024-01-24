4951623f34bcf6d40bb83ad1cbd77256166fda65
cristianreey <cris11salle@gmail.com>
2024-01-24 13:08:00 +0100

    login controller

---
+++ bd/controller/loginController.php
@@ -0,0 +1,49 @@
+<?php
+namespace controller;
+
+use bd\model\Carrito;
+use bd\model\Producto;
+use bd\model\Farmacia;
+
+require_once("../model/conexion.php");
+require_once("../model/producto.php");
+require_once("../model/carrito.php");
+
+// Comprobamos si la sesión ya está iniciada antes de intentar iniciarla
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Procesar datos del formulario
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $pdo = Farmacia::conectar();
+    // Recibir datos del formulario
+    $nombre = $_POST['nombre'];
+    $email = $_POST['gmail'];
+    $password = $_POST['contrasena'];
+    $fechaNacimiento = $_POST['fechaNac'];
+    $dni = $_POST['DNI'];
+    $telefono = $_POST['telefono'];
+
+
+    // Validar los datos (realiza validaciones más robustas según tus necesidades)
+    if (empty($nombre) || empty($email) || empty($password) || empty($fechaNacimiento) || empty($dni) || empty($telefono)) {
+        echo "Por favor, complete todos los campos.";
+    } else {
+        // Hash de la contraseña (mejora la seguridad almacenando contraseñas de manera segura)
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        // Insertar datos en la base de datos (ajusta la consulta según tu esquema)
+        $sql = "INSERT INTO clientes (nombre, gmail, contrasena, DNI, fechaNac, telefono) VALUES ('$nombre', '$email', '$hashedPassword', '$dni', '$fechaNacimiento', $telefono')";
+
+        if ($pdo->query($sql) === TRUE) {
+            echo "Registro exitoso";
+        } else {
+            echo "Error al registrar: " . $pdo->error;
+        }
+    }
+}
+
+// Cerrar la conexión a la base de datos
+$pdo->close();
+?>
